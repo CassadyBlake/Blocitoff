73426efe7c6f0e21211478549104925f4c333f13
CassadyBlake <cassady.lillstrom@gmail.com>
2018-05-12 08:14:32 -0700

    stuck on redirect to root on sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show/:id' => 'users#show', as: 'users_show'
+
+  get 'users/show/:id', to: 'users#show', as: 'users_show'
 
   devise_for :users
 
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
