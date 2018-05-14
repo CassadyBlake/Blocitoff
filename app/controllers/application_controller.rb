1e4af5f95b2662890f565d6b39e926fcbbb115de
CassadyBlake <cassady.lillstrom@gmail.com>
2018-05-14 13:41:38 -0700

    user story 3 userProfile: revision

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
 
   protected
     def after_sign_in_path_for(resource)
-      user_path(current_user)
+      root_path(current_user)
     end
 end
