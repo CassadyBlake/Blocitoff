4a191651ac5f1d7b78b6a015fc1f46ecd7a46d26
CassadyBlake <cassady.lillstrom@gmail.com>
2018-05-23 13:58:23 -0700

    update-markToDos: completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,31 +1,40 @@
 class ItemsController < ApplicationController
 
+
   def create
     @user = current_user
-    @item = Item.new
-    @item.name = params[:item][:name]
+    @item = Item.create(item_params)
     @item.user = current_user
 
-    if @item.save
-      flash[:notice] = "New item has been saved."
-      redirect_to user_path(current_user)
-    else
-      flash[:alert] = "There was an error saving the item, please try again."
-      render :create
+    @item.save
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
     end
+#    if @item.save
+#      flash[:notice] = "New item has been saved."
+#      redirect_to user_path(current_user)
+#    else
+#      flash[:alert] = "There was an error saving the item, please try again."
+#      render :create
+#    end
   end
 
   def destroy
     @user = current_user
     @item = Item.find(params[:id])
+    @item.destroy
 
-    if @item.destroy
-      flash[:notice] = "\"#{@item.name}\" completed."
-      redirect_to root_path
-    else
-      flash.now[:alert] = "There was an error completing the item. Please try again."
-      redirect_to root_path
-    end
+    @items = @user.items.all
+
+
+#      flash[:notice] = "\"#{@item.name}\" completed."
+#      redirect_to root_path
+#    else
+#      flash.now[:alert] = "There was an error completing the item. Please try again."
+#      redirect_to root_path
+#    end
+
   end
 
   def item_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @items = @user.items.all
+
+    respond_to do |format|
+      format.html
+      format.json
+    end 
   end
 end
